57f384279f2dc28e6b6dc4b430e45e00fcbd3d70
Takhir <guvalakat1603@gmail.com>
2024-08-14 22:56:20 +0500

    getAllProducts function is created

--- product-service/src/main/java/uz/tokhir/productservice/service/ProductService.java
+++ product-service/src/main/java/uz/tokhir/productservice/service/ProductService.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uz.tokhir.productservice.module.Product;
 import uz.tokhir.productservice.payload.ProductRequest;
+import uz.tokhir.productservice.payload.ProductResponse;
 import uz.tokhir.productservice.reposiroty.ProductRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,14 +16,18 @@ import uz.tokhir.productservice.reposiroty.ProductRepository;
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public void createProduct(ProductRequest productRequest) {
+    public Product createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.name())
                 .description(productRequest.description())
                 .price(productRequest.price())
                 .build();
-        productRepository.save(product);
+        Product save = productRepository.save(product);
         log.info("Product created successfully");
+        return save;
     }
 
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll().stream().map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice())).toList();
+    }
 }

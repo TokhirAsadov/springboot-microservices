f79f3c15869a63cb5edce4d5077f18cf75645190
Takhir <guvalakat1603@gmail.com>
2024-08-16 01:49:42 +0500

    existsBySkuCodeAndQuantityIsGreaterThanEqual function is created

--- inventory-service/src/main/java/uz/tokhir/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/uz/tokhir/inventoryservice/repository/InventoryRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.tokhir.inventoryservice.module.Inventory;
 
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+    Boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
 }

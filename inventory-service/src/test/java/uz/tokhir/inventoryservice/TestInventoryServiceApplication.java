74c553d3cfdad66ac54f37bd1143be838e9c3daf
Takhir <guvalakat1603@gmail.com>
2024-08-15 21:50:59 +0500

    inventory service is created

---
+++ inventory-service/src/test/java/uz/tokhir/inventoryservice/TestInventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package uz.tokhir.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(InventoryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}

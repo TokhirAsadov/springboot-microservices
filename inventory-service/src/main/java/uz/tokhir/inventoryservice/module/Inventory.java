460330b76a9ca5a1fd77d0f6e2e73334dee735ae
Takhir <guvalakat1603@gmail.com>
2024-08-16 01:46:18 +0500

    Inventory entity is created

---
+++ inventory-service/src/main/java/uz/tokhir/inventoryservice/module/Inventory.java
@@ -0,0 +1,21 @@
+package uz.tokhir.inventoryservice.module;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_inventory")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+    private String skuCode;
+}

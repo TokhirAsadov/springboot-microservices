e43bd69c5e69c0aa88e171f997d69e6c39229fcd
Takhir <guvalakat1603@gmail.com>
2024-08-15 20:32:16 +0500

    order service module is added

---
+++ order-service/src/test/java/uz/tokhir/orderservice/TestOrderServiceApplication.java
@@ -0,0 +1,11 @@
+package uz.tokhir.orderservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrderServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}

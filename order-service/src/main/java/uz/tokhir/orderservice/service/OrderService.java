2e9ca67fced9d55eb3731683de11f30f9f0b4a04
Takhir <guvalakat1603@gmail.com>
2024-08-15 20:51:17 +0500

    placeOrder function is created

--- order-service/src/main/java/uz/tokhir/orderservice/service/OrderService.java
+++ order-service/src/main/java/uz/tokhir/orderservice/service/OrderService.java
@@ -1,8 +1,25 @@
 package uz.tokhir.orderservice.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.tokhir.orderservice.module.Order;
+import uz.tokhir.orderservice.payload.OrderRequest;
+import uz.tokhir.orderservice.repository.OrderRepository;
+
+import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
 
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+        orderRepository.save(order);
+    }
 }

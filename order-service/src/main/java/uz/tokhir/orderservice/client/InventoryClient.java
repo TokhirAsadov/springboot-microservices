f01f57aaf73366fcf9ffd0f8d0c4d3fe02514ec1
Takhir <guvalakat1603@gmail.com>
2024-08-19 11:58:38 +0500

    isInStock is created

--- order-service/src/main/java/uz/tokhir/orderservice/client/InventoryClient.java
+++ order-service/src/main/java/uz/tokhir/orderservice/client/InventoryClient.java
@@ -8,5 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "inventory",url = "http://localhost:8082")
 public interface InventoryClient {
 
-
+    @RequestMapping(method = RequestMethod.GET,value = "/api/inventory")
+    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }

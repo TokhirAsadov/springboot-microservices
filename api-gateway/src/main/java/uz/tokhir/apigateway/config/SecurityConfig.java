52ffb44820349bdc3ebb377f81c4ce295edfe000
Takhir <guvalakat1603@gmail.com>
2024-08-19 15:44:48 +0500

    SecurityConfig is created

---
+++ api-gateway/src/main/java/uz/tokhir/apigateway/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package uz.tokhir.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth -> auth.anyRequest()
+                .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}

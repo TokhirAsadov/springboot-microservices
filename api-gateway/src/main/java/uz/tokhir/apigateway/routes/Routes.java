a807549700c11d5436ef1b4095acf1a4a648aa71
Takhir <guvalakat1603@gmail.com>
2024-08-19 14:42:24 +0500

    Routes is created and product-service is added to api-gateway

---
+++ api-gateway/src/main/java/uz/tokhir/apigateway/routes/Routes.java
@@ -0,0 +1,20 @@
+package uz.tokhir.apigateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        return GatewayRouterFunctions.route("product_service")
+                .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
+                .build();
+    }
+}

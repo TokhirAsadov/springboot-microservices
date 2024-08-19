0608981301e84654f0529857b21677f6f9622465
Takhir <guvalakat1603@gmail.com>
2024-08-19 13:59:51 +0500

    inventory client is used

--- order-service/src/main/java/uz/tokhir/orderservice/service/OrderService.java
+++ order-service/src/main/java/uz/tokhir/orderservice/service/OrderService.java
@@ -2,6 +2,7 @@ package uz.tokhir.orderservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.tokhir.orderservice.client.InventoryClient;
 import uz.tokhir.orderservice.module.Order;
 import uz.tokhir.orderservice.payload.OrderRequest;
 import uz.tokhir.orderservice.repository.OrderRepository;
@@ -13,13 +14,22 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+        Boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
+
+        if (isProductInStock){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        }
+        else {
+            throw new RuntimeException("Product with SkuCode "+orderRequest.skuCode()+" is not in stock");
+        }
+
     }
 }

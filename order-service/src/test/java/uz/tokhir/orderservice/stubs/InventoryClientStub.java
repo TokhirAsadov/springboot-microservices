23e90322402416aeaa0b878c716ae2e4a2395c19
Takhir <guvalakat1603@gmail.com>
2024-08-19 14:07:52 +0500

    stubInventoryCall function is created

--- order-service/src/test/java/uz/tokhir/orderservice/stubs/InventoryClientStub.java
+++ order-service/src/test/java/uz/tokhir/orderservice/stubs/InventoryClientStub.java
@@ -1,4 +1,15 @@
 package uz.tokhir.orderservice.stubs;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
 public class InventoryClientStub {
+
+    public void stubInventoryCall(String skuCode,Integer quantity){
+        stubFor(get(urlEqualTo("/api/inventory?skuCode="+skuCode+"&quantity="+quantity))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("true")));
+    }
+
 }

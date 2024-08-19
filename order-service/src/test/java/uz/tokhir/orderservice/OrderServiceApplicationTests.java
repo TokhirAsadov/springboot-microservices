127d44c6a42e7fa97689ff23ef88f0efd541e6a3
Takhir <guvalakat1603@gmail.com>
2024-08-19 14:01:56 +0500

    @AutoConfigureWireMock(port = 0) is used

--- order-service/src/test/java/uz/tokhir/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/uz/tokhir/orderservice/OrderServiceApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 import org.hamcrest.Matchers;
@@ -14,6 +15,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection

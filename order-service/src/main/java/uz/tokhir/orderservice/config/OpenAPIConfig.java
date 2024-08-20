431e1fed2ac7d2fec922da2784abf35f6ac02917
Takhir <guvalakat1603@gmail.com>
2024-08-20 11:32:33 +0500

    orderServiceOpenAPI is created

--- order-service/src/main/java/uz/tokhir/orderservice/config/OpenAPIConfig.java
+++ order-service/src/main/java/uz/tokhir/orderservice/config/OpenAPIConfig.java
@@ -1,7 +1,25 @@
 package uz.tokhir.orderservice.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI orderServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Order Service API by Tokhir")
+                        .description("This is the REST API for Order Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("You can refer to the Order Service Wiki Documentation")
+                        .url("https://github.com/TokhirAsadov/springboot-microservices"));
+    }
+
 }

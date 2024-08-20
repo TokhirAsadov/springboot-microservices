2a47cc01796d5db6126ad314acd5de9947a0d717
Takhir <guvalakat1603@gmail.com>
2024-08-20 11:17:25 +0500

    swagger configuration

--- product-service/src/main/java/uz/tokhir/productservice/config/OpenAPIConfig.java
+++ product-service/src/main/java/uz/tokhir/productservice/config/OpenAPIConfig.java
@@ -1,8 +1,24 @@
 package uz.tokhir.productservice.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
 
+    @Bean
+    public OpenAPI productServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Product Service API by Tokhir")
+                        .description("This is the REST API for Product Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("You can refer to the Product Service Wiki Documentation")
+                        .url("https://github.com/TokhirAsadov/springboot-microservices"));
+    }
 }

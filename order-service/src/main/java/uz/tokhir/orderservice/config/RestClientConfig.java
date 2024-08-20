a5c792faaa6be157c78ac17db60149988aecbfd0
Takhir <guvalakat1603@gmail.com>
2024-08-20 16:04:43 +0500

    time out is added

--- order-service/src/main/java/uz/tokhir/orderservice/config/RestClientConfig.java
+++ order-service/src/main/java/uz/tokhir/orderservice/config/RestClientConfig.java
@@ -1,12 +1,17 @@
 package uz.tokhir.orderservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import uz.tokhir.orderservice.client.InventoryClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,10 +23,18 @@ public class RestClientConfig {
     public InventoryClient inventoryClient(){
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
     }
 
 //    RestClient defaultClient = RestClient.create();

63d51bc1e803e41ff5bb5db49ed5f6ff84327339
Takhir <guvalakat1603@gmail.com>
2024-08-20 15:38:37 +0500

    @Bean

--- order-service/src/main/java/uz/tokhir/orderservice/config/RestClientConfig.java
+++ order-service/src/main/java/uz/tokhir/orderservice/config/RestClientConfig.java
@@ -1,6 +1,7 @@
 package uz.tokhir.orderservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
@@ -13,6 +14,7 @@ public class RestClientConfig {
     @Value("${inventory.url}")
     private String inventoryServiceUrl;
 
+    @Bean
     public InventoryClient inventoryClient(){
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)

414f8e8a097c5220e78677477e800627a96d9597
Takhir <guvalakat1603@gmail.com>
2024-08-20 12:04:51 +0500

    freeResourceUrls is opened

--- api-gateway/src/main/java/uz/tokhir/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/uz/tokhir/apigateway/config/SecurityConfig.java
@@ -9,10 +9,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] freeResourceUrls = {
+            "/swagger-ui.html",
+            "/tohir/swagger-ui.html",
+            "/tohir/swagger-ui/**",
+            "/tohir/swagger-resources/**",
+            "/v3/api-docs/**",
+            "/api-docs/**",
+            "/tohir/api-docs/**",
+            "/aggregate/**",
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth -> auth.anyRequest()
-                .authenticated())
+        return http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(freeResourceUrls)
+                        .permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }

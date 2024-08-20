ed7f666f4a7ddeda9c5a5a96a5e5497e589dcedb
Takhir <guvalakat1603@gmail.com>
2024-08-20 11:57:25 +0500

    productServiceSwaggerRoute

--- api-gateway/src/main/java/uz/tokhir/apigateway/routes/Routes.java
+++ api-gateway/src/main/java/uz/tokhir/apigateway/routes/Routes.java
@@ -1,5 +1,6 @@
 package uz.tokhir.apigateway.routes;
 
+import org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,14 @@ public class Routes {
     public RouterFunction<ServerResponse> productServiceRoute() {
         return GatewayRouterFunctions.route("product_service")
                 .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(FilterFunctions.setPath("/tohir/api-docs"))
                 .build();
     }
 

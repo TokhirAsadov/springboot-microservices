0849e6266a11185a9ace1ad0b5c7b473020d19fc
Takhir <guvalakat1603@gmail.com>
2024-08-26 11:34:55 +0500

    keycloak configuration is used

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,142 @@
-import AppContent from "./components/AppContent";
+import React, {useState} from "react";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import '/node_modules/primeflex/primeflex.css'
+import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
 
-function App() {
-  return (
-      <div className="row">
-          <div className="col">
-              <AppContent/>
-          </div>
-      </div>
-  );
+
+import {httpClient} from "./HttpClient";
+import {kc} from "./Keycloak";
+
+/*
+  Init Options
+*/
+let initOptions = {
+    url: "http://localhost:8181/",
+    realm: "spring-microservices-security-realm",
+    clientId: "react-client",
+    redirectUri: "http://localhost:3000/"
 }
+
+kc.init({
+    onLoad: 'login-required', // Supported values: 'check-sso' , 'login-required'
+    checkLoginIframe: true,
+    pkceMethod: 'S256'
+}).then((auth) => {
+    if (!auth) {
+        window.location.reload();
+    } else {
+        /* Remove below logs if you are using this on production */
+        console.info("Authenticated");
+        console.log('auth', auth)
+        console.log('Keycloak', kc)
+        console.log('Access Token', kc.token)
+        console.log('parsed', kc.tokenParsed)
+
+        /* http client will use this header in every request it sends */
+        httpClient.defaults.headers.common['Authorization'] = `Bearer ${kc.token}`;
+
+        kc.onTokenExpired = () => {
+            console.log('token expired')
+        }
+    }
+}, () => {
+    /* Notify the user if necessary */
+    console.error("Authentication Failed");
+});
+
+const App = () =>{
+    const [infoMessage, setInfoMessage] = useState('');
+
+    /* To demonstrate : http client adds the access token to the Authorization header */
+    const callBackend = () => {
+        httpClient.get('https://mockbin.com/request')
+
+    };
+
+    // const username = kc?.tokenParsed?.preferred_username;
+
+    return (
+        <div className="App">
+            <div className='grid'>
+                <div className='col-12'>
+                    <h1>My Secured React App</h1>
+                </div>
+            </div>
+            <div className="grid">
+
+            </div>
+
+            <div className='grid'>
+                <div className='col-1'></div>
+                <div className='col-2'>
+                    <div className="col">
+                        <Button onClick={() => { setInfoMessage(kc.authenticated ? 'Authenticated: TRUE -> '+kc?.tokenParsed?.preferred_username : 'Authenticated: FALSE') }}
+                                className="m-1 custom-btn-style"
+                                label='Is Authenticated' />
+
+                        <Button onClick={() => { kc.login() }}
+                                className='m-1 custom-btn-style'
+                                label='Login'
+                                severity="success" />
+
+                        <Button onClick={() => { setInfoMessage(kc.token) }}
+                                className="m-1 custom-btn-style"
+                                label='Show Access Token'
+                                severity="info" />
+
+                        <Button onClick={() => { setInfoMessage(JSON.stringify(kc.tokenParsed)) }}
+                                className="m-1 custom-btn-style"
+                                label='Show Parsed Access token'
+                                severity="warning" />
+
+                        <Button onClick={() => { setInfoMessage(kc.isTokenExpired(5).toString()) }}
+                                className="m-1 custom-btn-style"
+                                label='Check Token expired'
+                                severity="info" />
+
+                        <Button onClick={() => { kc.updateToken(10).then((refreshed) => { setInfoMessage('Token Refreshed: ' + refreshed.toString()) }, (e) => { setInfoMessage('Refresh Error') }) }}
+                                className="m-1 custom-btn-style"
+                                label='Update Token (if about to expire)' />  {/** 10 seconds */}
+
+                        <Button onClick={callBackend}
+                                className='m-1 custom-btn-style'
+                                label='Send HTTP Request'
+                                severity="success" />
+
+                        <Button onClick={() => { kc.logout(initOptions) }}
+                                className="m-1 custom-btn-style"
+                                label='Logout'
+                                severity="danger" />
+
+                        <Button onClick={() => { setInfoMessage(kc.hasRealmRole('admin').toString()) }}
+                                className="m-1 custom-btn-style"
+                                label='has realm role "Admin"'
+                                severity="info" />
+
+                        <Button onClick={() => { setInfoMessage(kc.hasResourceRole('test').toString()) }}
+                                className="m-1 custom-btn-style"
+                                label='has client role "test"'
+                                severity="info" />
+
+                    </div>
+                </div>
+                <div className='col-6'>
+
+                    <Card>
+                        <p style={{ wordBreak: 'break-all' }} id='infoPanel'>
+                            {infoMessage}
+                        </p>
+                    </Card>
+                </div>
+
+                <div className='col-2'></div>
+            </div>
+
+
+
+        </div>)
+} ;
 
 export default App;

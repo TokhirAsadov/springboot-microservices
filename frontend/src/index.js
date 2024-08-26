bbce18070aec426915c1edce3b32c4146febfd54
Takhir <guvalakat1603@gmail.com>
2024-08-26 16:58:03 +0500

    KeycloakProvider is used

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'react-toastify/dist/ReactToastify.css';
+import {KeycloakProvider} from "./KeycloakContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <KeycloakProvider>
+        <App />
+    </KeycloakProvider>,
 );

84cb71aad8ea510672534b169bdd874f2608b6ed
Takhir <guvalakat1603@gmail.com>
2024-08-26 16:56:49 +0500

    Navbar is created

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { kc } from "../Keycloak";
+
+const Nav = ({ isAuthenticated, login, logout }) => {
+    return (
+        <div className={'flex gap-4 justify-between px-8 py-4 bg-blue-50'}>
+            <div className={'font-bold text-blue-500 text-ls'}>MICROSERVICES</div>
+            {
+                isAuthenticated ? (
+                    <div className={'flex flex-col gap-1'}>
+                        <div className={'text-blue-600'}>Hi, {kc?.tokenParsed?.preferred_username}</div>
+                        <hr/>
+                        <button className={'font-bold text-gray-700 text-xs hover:text-blue-400'} onClick={() => logout()}>Logout</button>
+                    </div>
+                ) : (
+                    <button onClick={() => login()}>Login</button>
+                )
+            }
+        </div>
+    );
+};
+
+export default Nav;

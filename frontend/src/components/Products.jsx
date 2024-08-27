55220ff9c565c11896f470e03203d9051576199b
Takhir <guvalakat1603@gmail.com>
2024-08-27 15:16:13 +0500

    navigation is added

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,9 +1,14 @@
 import Product from "./Product";
+import { useNavigate } from "react-router-dom";
 
 const Products = ({products}) => {
+    const navigate = useNavigate();
     return (
         <div className={'h-[calc(100vh - 64px)] bg-cyan-50 flex flex-col gap-8 px-16 py-4'}>
-            <h1 className={'text-3xl'}>Products({products?.length}):</h1>
+            <div className="flex justify-between items-center">
+                <h1 className={'text-3xl'}>Products({products?.length}):</h1>
+                <button onClick={()=>{navigate("/add-product")}} className="flex items-center bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-400">Create Product</button>
+            </div>
             {products?.length === 0 && <h1 className={'text-lg text-red-600'}>Not found products..</h1>}
             <hr/>
             <div className={'flex flex-wrap gap-2'}>

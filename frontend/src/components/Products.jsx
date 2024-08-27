f360c58b699f645dfd36a62b2a208b9e23f8d8cf
Takhir <guvalakat1603@gmail.com>
2024-08-27 15:58:52 +0500

    changing

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -7,7 +7,7 @@ const Products = ({products}) => {
         <div className={'h-[calc(100vh - 64px)] bg-cyan-50 flex flex-col gap-8 px-16 py-4'}>
             <div className="flex justify-between items-center">
                 <h1 className={'text-3xl'}>Products({products?.length}):</h1>
-                <button onClick={()=>{navigate("/add-product")}} className="flex items-center bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-400">Create Product</button>
+                <button onClick={()=>{navigate("/add-product")}} className="flex items-center bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-400">+ Create Product</button>
             </div>
             {products?.length === 0 && <h1 className={'text-lg text-red-600'}>Not found products..</h1>}
             <hr/>

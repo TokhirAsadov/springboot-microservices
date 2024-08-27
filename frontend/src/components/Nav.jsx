02804c354b50ff136d61d5f66202bafe9e3bbae2
Takhir <guvalakat1603@gmail.com>
2024-08-27 16:07:00 +0500

    navigation is used

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { kc } from "../Keycloak";
+import {useNavigate} from "react-router-dom";
 
 const Nav = ({ isAuthenticated, login, logout }) => {
+    const navigate = useNavigate();
     return (
         <div className={'h-16 px-16 flex gap-4 justify-between items-center bg-blue-50'}>
-            <div className={'font-bold text-blue-500 text-ls'}>MICROSERVICES</div>
+            <button onClick={()=>{navigate("/")}} className={'font-bold text-blue-500 text-ls'}>MICROSERVICES</button>
             {
                 isAuthenticated ? (
                     <div className={'flex flex-col gap-1'}>

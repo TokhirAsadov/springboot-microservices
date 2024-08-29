4732d32cdf6b0ce89b1d28b037541b73fab6e580
Takhir <guvalakat1603@gmail.com>
2024-08-29 14:22:16 +0500

    NotificationService service is created

---
+++ notification-service/src/main/java/uz/tokhir/notification/service/NotificationService.java
@@ -0,0 +1,11 @@
+package uz.tokhir.notification.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+}

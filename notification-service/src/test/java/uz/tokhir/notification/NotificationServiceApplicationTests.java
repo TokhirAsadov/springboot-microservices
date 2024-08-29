dabb7cc98a54a2e873fd0f62b1f854538952f3a2
Takhir <guvalakat1603@gmail.com>
2024-08-29 14:12:50 +0500

    notification module is added

---
+++ notification-service/src/test/java/uz/tokhir/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package uz.tokhir.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-service/src/test/java/uz/tokhir/notification/TestNotificationServiceApplication.java
@@ -0,0 +1,11 @@
+package uz.tokhir.notification;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(NotificationServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}

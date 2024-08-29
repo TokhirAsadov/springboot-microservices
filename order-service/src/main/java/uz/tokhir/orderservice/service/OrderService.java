67ca38ac8eee5c9e45a77154f893639fea2c7828
Takhir <guvalakat1603@gmail.com>
2024-08-29 14:01:35 +0500

    send request to kafka

--- order-service/src/main/java/uz/tokhir/orderservice/service/OrderService.java
+++ order-service/src/main/java/uz/tokhir/orderservice/service/OrderService.java
@@ -1,8 +1,12 @@
 package uz.tokhir.orderservice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import uz.tokhir.orderservice.client.InventoryClient;
+import uz.tokhir.orderservice.event.OrderPlacedEvent;
 import uz.tokhir.orderservice.module.Order;
 import uz.tokhir.orderservice.payload.OrderRequest;
 import uz.tokhir.orderservice.repository.OrderRepository;
@@ -13,8 +17,10 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest){
         Boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
@@ -26,6 +32,12 @@ public class OrderService {
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+
+            // Send the message to Kafka Topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(),orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         }
         else {
             throw new RuntimeException("Product with SkuCode "+orderRequest.skuCode()+" is not in stock");

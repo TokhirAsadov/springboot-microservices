4b81b821daa42209a2d6be5bb9295f6ee734bd0b
Takhir <guvalakat1603@gmail.com>
2024-08-29 12:46:53 +0500

    UserDetails record is added

--- order-service/src/main/java/uz/tokhir/orderservice/payload/OrderRequest.java
+++ order-service/src/main/java/uz/tokhir/orderservice/payload/OrderRequest.java
@@ -2,5 +2,6 @@ package uz.tokhir.orderservice.payload;
 
 import java.math.BigDecimal;
 
-public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity) {
+public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity,UserDetails userDetails) {
+    public record UserDetails(String email, String firstName, String lastName) {}
 }
